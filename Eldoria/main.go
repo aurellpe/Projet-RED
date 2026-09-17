3a26ec1d6ceb8fd3a59f2e8f56f3c3c081d21ec0
cyael <yael.castan@ynov.com>
2026-09-17 22:06:01 +0200

    quete 2

---
+++ Eldoria/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Mon premier jeu")
+}
+
+func (g *Game) Layout(width, height int) (int, int) {
+	return 800, 600
+}
+
+func main() {
+	ebiten.SetWindowSize(800, 600)
+	ebiten.SetWindowTitle("Eldoria")
+
+	game := &Game{}
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
