95183306099238c594756c4311dbc419986229b4
aurellpe <aurelien.lepere@ynov.com>
2026-09-18 11:46:44 +0200

    init

--- src/main.go
+++ src/main.go
@@ -1,17 +1,65 @@
 package main
 
-import "main/structure"
+import (
+	"fmt"
+	"main/structure"
+)
 
-func main() {
-	perso := structure.Personnage{
-		Nom:                "DE Bergerac",
-		Prenom:             "Dartagnan",
-		Age:                26,
-		Classe:             "Legionnaire",
-		Niveau:             1,
-		PointsDeVieMaximum: 10000,
-		PointsDeVieActuels: 150,
-		Inventaire:         []string{},
+func initCharacter(nom string, prenom string, age int, classe string, niveau int, pointsDeVieMaximum int, pointsDeVieActuels int, inventaire []string) structure.Personnage {
+	return structure.Personnage{
+		Nom:                 nom,
+		Prenom:              prenom,
+		Age:                 age,
+		Classe:              classe,
+		Niveau:              niveau,
+		PointsDeVieMaximum:  pointsDeVieMaximum,
+		PointsDeVieActuels:  pointsDeVieActuels,
+		Inventaire:          inventaire,
+	}
+}
+
+func displayInfo(perso structure.Personnage) {
+	fmt.Println("===== PERSONNAGE =====")
+	fmt.Println("Nom :", perso.Nom)
+	fmt.Println("Prénom :", perso.Prenom)
+	fmt.Println("Âge :", perso.Age)
+	fmt.Println("Classe :", perso.Classe)
+	fmt.Println("Niveau :", perso.Niveau)
+	fmt.Println("Points de vie :", perso.PointsDeVieActuels, "/", perso.PointsDeVieMaximum)
+}
+
+func accessInventory(perso structure.Personnage) {
+	fmt.Println("===== INVENTAIRE =====")
+
+	if len(perso.Inventaire) == 0 {
+		fmt.Println("L'inventaire est vide.")
+		return
 	}
 
+	for i, objet := range perso.Inventaire {
+		fmt.Println(i+1, "-", objet)
+	}
+}
+
+func main() {
+
+	inventaire := []string{
+	}
+
+	perso := initCharacter(
+		"DE Bergerac",
+		"Dartagnan",
+		26,
+		"Elfe",
+		1,
+		100,
+		40,
+		inventaire,
+	)
+
+	displayInfo(perso)
+
+	fmt.Println()
+
+	accessInventory(perso)
 }
