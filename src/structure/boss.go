1aeab1ce8ca752b2b462ddc6a746f8f7f34298cb
aurellpe <aurelien.lepere@ynov.com>
2026-09-21 16:24:50 +0200

    code

---
+++ src/structure/boss.go
@@ -0,0 +1,25 @@
+package structure
+
+type Boss struct {
+    Nom                string
+    Niveau             int
+    PointsDeVieMaximum int
+    PointsDeVieActuels int
+    Degats             int
+    Initiative         int
+    RecompenseXP       int
+    RecompenseArgent   int
+}
+
+func NewBoss() Boss {
+    return Boss{
+        Nom:                "Azrakar, Seigneur des Ombres",
+        Niveau:             5,
+        PointsDeVieMaximum: 500,
+        PointsDeVieActuels: 500,
+        Degats:             35,
+        Initiative:         15,
+        RecompenseXP:       200,
+        RecompenseArgent:   100,
+    }
+}
